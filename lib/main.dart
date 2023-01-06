8991d2e8793969af2e0656e0e50912395de71235
Azhar Razak <azhar.rayray@gmail.com>
2023-01-06 12:59:05 +0200

    Completed and Updated to latest Flutter version

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Center(child: Text('Dice App')),
+          backgroundColor: Colors.lightBlue,
         ),
         body: DicePage(),
       ),
@@ -15,9 +18,54 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+// class DicePage extends StatelessWidget {
+//
+//
+// }
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int RandomDieLeft = 1;
+  int RandomDieRight = 1;
+
+  void setDice() {
+    RandomDieRight = Random().nextInt(6) + 1;
+    RandomDieLeft = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  setDice();
+                });
+              },
+              child: Image.asset('images/dice$RandomDieLeft.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  setDice();
+                });
+              },
+              child: Image.asset('images/dice$RandomDieRight.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
